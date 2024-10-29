0a53537fa27c9df2071a9b1604dc461aa08f1c86
Krishneshwaran <krishneshwarand@gmail.com>
2024-10-29 14:35:17 +0530

    Organized Working

--- Backend/api_handler.py
+++ Backend/api_handler.py
@@ -2,7 +2,7 @@ import requests
 import base64
 from config import API_URL, HEADERS, QUERY_STRING
 
-def execute_code(code, language, stdin_input, selected_row, df):
+def execute_code(code, language, stdin_input, selected_row, problems):
     encoded_code = base64.b64encode(code.encode()).decode()
     encoded_stdin = base64.b64encode(stdin_input.encode()).decode()
     language_id = 71 if language == "python" else 52 if language == "c_cpp" else 62
@@ -16,6 +16,8 @@ def execute_code(code, language, stdin_input, selected_row, df):
     response = requests.post(API_URL, json=payload, headers=HEADERS, params=QUERY_STRING)
     result = response.json()
     output = base64.b64decode(result["stdout"]).decode().strip() if "stdout" in result else ""
-    expected_output = str(df.at[selected_row, 'Output']).strip()
+
+    # Retrieve the expected output from the problems list
+    expected_output = str(problems[selected_row]["samples"][0]["output"]).strip()
     
     return output, expected_output
