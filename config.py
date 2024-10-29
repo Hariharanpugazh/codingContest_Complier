0908e840b25c88b6fed649dc52c85c2ab9f5ffc8
Naresh-Developr <naresh.k.personal@gmail.com>
2024-10-29 12:48:38 +0530

    fix1

--- config.py
+++
@@ -1,7 +0,0 @@
-API_URL = "https://judge029.p.rapidapi.com/submissions"
-HEADERS = {
-    "x-rapidapi-key": "adfbe3a184mshb52145137acec4dp1405edjsne54330b823c0",
-    "x-rapidapi-host": "judge029.p.rapidapi.com",
-    "Content-Type": "application/json"
-}
-QUERY_STRING = {"base64_encoded": "true", "wait": "true", "fields": "*"}
--- utils.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-import streamlit as st
-
-@st.cache_data
-def load_test_cases():
-    file_path = pd.read_csv("test_cases.csv")
-    return file_path
