e7613b30171339cf2ae085123df525bfec6dc0bf
Naresh-Developr <naresh.k.personal@gmail.com>
2024-11-05 11:57:05 +0530

    small update path

--- Backend/coding_platform/settings.py
+++ Backend/coding_platform/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "compile",
+    "problems",
+    "contest_details",
     "corsheaders",  # Add this line
 ]
 
